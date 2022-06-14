2ec9f6e92145065d1f0d8086d2fb3e48418f8ff4
murilonerdx <mu-silva@outlook.com>
2022-06-14 16:52:23 -0300

    [ADD] Projeto inicial

---
+++ src/main/java/com/murilonerdx/gerenciador/NtConsultApplication.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtConsultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtConsultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/murilonerdx/gerenciador/NtConsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtConsultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
